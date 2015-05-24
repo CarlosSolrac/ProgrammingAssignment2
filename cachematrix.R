7db96e2a07f4723a4b947a65be8d51d918de12da
CarlosSolrac <carlos@supalynx.com>
2015-05-24 15:44:44 -0700

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,16 +2,27 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This follows the same algorithm for cacheMean
 makeCacheMatrix <- function(x = matrix()) {
+  ## init data
   m <- NULL
+  
+  ## sets the matrix and sets the cached inverse to null
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
+  
+  ## get the current matrix
   get <- function() x
+  
+  ## This actually caches the inverse matrix in a separate environment
   setInverse <- function(inverse) m <<- inverse
+  
+  ## get the inverse matrix. If the inverse has not been calculated, then we return null 
   getInverse <- function() m
+  
+  ## 
   list(set = set, get = get,
        setInverse = setInverse,
        getInverse = getInverse)
@@ -21,7 +32,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   m <- x$getInverse()
   if(!is.null(m)) {
     message("getting cached data")
@@ -31,6 +42,37 @@ cacheSolve <- function(x, ...) {
   m <- solve(data, ...)
   x$setInverse(m)
   m
+  
+}
+
+
+testCode <- function() {
+  ## build an invertable matrix
+  v<-makeCacheMatrix(rbind(c(1, -1/4), c(-1/4, 1)))
+  
+  ## make sure it built properly
+  print("matrix")
+  print(v$get())
+  cat("\n")
+  
+  print("inverse is null")
+  print(v$getInverse())
+  cat("\n")
+  
+  ## init the cache
+  print("inverse is null. First time attempting. Inverse will be calculated.")
+  print(cacheSolve(v))
+  cat("\n")
+  
+  ## call it again to make sure it was retrieved from the cache
+  print("Retrieved from cache")
+  print(cacheSolve(v))
+  cat("\n")
+  
+  ## verify that we really did calculate the inverse by making sure we got an identity matrix
+  print("Should return identity matrix")
+  print(cacheSolve(v) %*% v$get())
+  cat("\n")
   
 }
 
